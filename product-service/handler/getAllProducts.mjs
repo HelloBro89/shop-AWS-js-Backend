35a7265da5b599710c63d765f952f17e76dd893c
Pavel <pavelvaloshyn89@gmail.com>
2021-09-02 21:39:47 +0200

    refactor: deleted extra code

--- product-service/handler/getAllProducts.mjs
+++ product-service/handler/getAllProducts.mjs
@@ -3,11 +3,9 @@ import productList from './productList.mjs';
 
 export const getAllProducts = async event => {
 
-   console.log("Get all products: ", event)
+   console.log("Get all products: ", event);
 
    try {
-
-    if (productList.length > 0) {
      return {
        statusCode: 200,
         headers: {
@@ -17,17 +15,44 @@ export const getAllProducts = async event => {
 
        body: JSON.stringify(productList),
     };
-  } 
-     return {
-        tatusCode: 404,
-       headers: {
-          'Access-Control-Allow-Origin': '*',
-         'Access-Control-Allow-Credentials': true,
-    },
-   body: JSON.stringify({message: 'Product not found'}),
-    };
-
-    } catch (e) {
+   } catch (e) {
        console.log("Error: ", e);
   }
 };
+
+
+
+
+// 'use strict';
+// import productList from './productList.mjs';
+// 
+// export const getAllProducts = async event => {
+// 
+//    console.log("Get all products: ", event)
+// 
+//    try {
+// 
+//     if (productList.length > 0) {
+//      return {
+//        statusCode: 200,
+//         headers: {
+//          'Access-Control-Allow-Origin': '*',
+//          'Access-Control-Allow-Credentials': true,
+//        },
+// 
+//        body: JSON.stringify(productList),
+//     };
+//   } 
+//      return {
+//         tatusCode: 404,
+//        headers: {
+//           'Access-Control-Allow-Origin': '*',
+//          'Access-Control-Allow-Credentials': true,
+//     },
+//    body: JSON.stringify({message: 'Product not found'}),
+//     };
+// 
+//     } catch (e) {
+//        console.log("Error: ", e);
+//   }
+// };
