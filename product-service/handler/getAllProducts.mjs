bdcc709713081ad62d96c8afcb916920b82a3f61
Pavel <pavelvaloshyn89@gmail.com>
2021-09-03 20:46:30 +0200

    feat: added request to DB getall and getId products

--- product-service/handler/getAllProducts.mjs
+++ product-service/handler/getAllProducts.mjs
@@ -1,20 +1,21 @@
 'use strict';
-import PG from 'pg';
-const { Client } = PG;
-
-const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
-
-const dbOptions = {
-   host: PG_HOST,
-   port: PG_PORT,
-   database: PG_DATABASE,
-   user: PG_USERNAME,
-   password: PG_PASSWORD,
-   ssl: {
-      rejectUnauthorized: false
-   },
-   connectionTimeoutMillis: 5000
-};
+import {dbOptions, Client} from './connection/connection.mjs';
+// import PG from 'pg';
+// const { Client } = PG;
+// 
+// const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+// 
+// const dbOptions = {
+//    host: PG_HOST,
+//    port: PG_PORT,
+//    database: PG_DATABASE,
+//    user: PG_USERNAME,
+//    password: PG_PASSWORD,
+//    ssl: {
+//       rejectUnauthorized: false
+//    },
+//    connectionTimeoutMillis: 5000
+// };
 
 export const getAllProducts = async event => {
    const client = new Client(dbOptions);
@@ -23,7 +24,8 @@ export const getAllProducts = async event => {
    console.log("**********Get logs of: ", event);
 
    try {
-      const {rows: products} = await client.query(`select * from products`);
+      const {rows: products} = await client.query(`select id, count, price, title, description
+      from products p left join stocks s on p.id = s.product_id`);
       return {
          statusCode: 200,
          headers: {
--- product-service/handler/getProductsById.mjs
+++ product-service/handler/getProductsById.mjs
@@ -1,36 +1,39 @@
 'use strict';
-import productList from './productList.mjs';
+import {dbOptions, Client} from './connection/connection.mjs';
 
 export const getProductsById = async event => {
+  const client = new Client(dbOptions);
+  await client.connect();
 
-  console.log("Get product by ID: ", event);
-
-try {
-  
-  const params = await event.pathParameters.productId;
-  const productById = productList.find(item => item.id === params);
-  
-  if (productById){
+  console.log("***********Get product by ID: ", event);
+ 
+  try {
+    const params = await event.pathParameters.productId;
+    
+    const {rows: productById} = await client.query(`select id, count, price, title, description from products p left join stocks s on p.id = s.product_id where id in ('${params}')`);
+    if (productById){
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+          },
+          body: JSON.stringify(productById),
+         };
+      }
     return {
-      statusCode: 200,
+      statusCode: 404,
       headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true,
       },
-      body: JSON.stringify(productById),
+      body: JSON.stringify({message: 'Product not found'}),
     };
-  } 
-  return {
-    statusCode: 404,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify({message: 'Product not found'}),
-  };
     
-    } catch (e) {
-  console.log("Error: ", e);
+   } catch (e) {
+     console.log("***********Error: ", e);
+     } finally {
+      client.end()
+    }
   }
-};
   
