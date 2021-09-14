386cdebbe82937d8529699c2de7da46781cddb92
Pavel <pavelvaloshyn89@gmail.com>
2021-09-14 20:24:55 +0200

    init and added test-lambda on import-serwis

--- import-service/handler/importProductsFile.js
+++ import-service/handler/importProductsFile.js
@@ -1,12 +1,11 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+export const importProductsFile = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'All right!!!'
       },
       null,
       2
