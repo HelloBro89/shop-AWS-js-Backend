ca600147cb4043861a07df9adea2cb5c9252d1a7
Pavel <pavelvaloshyn89@gmail.com>
2021-10-03 12:27:48 +0200

    refactor: changed structure of micro-service, sqsqueue moded to product

--- product-service/__tests__/test.spec.js
+++ product-service/__tests__/test.spec.js
@@ -1,51 +1,144 @@
-import { getProductsById } from "../handler/getProductsById.mjs";
-import {getAllProducts}  from "../handler/getAllProducts.mjs";
-import productList from '../handler/productList.mjs'; 
+import AWSMock from 'aws-sdk-mock';
+import AWS from 'aws-sdk';
+import { catalogBatchProcess } from '../handler/catalogBatchProcess.js';
 
-  test('getAllProducts lambda: get all product', async () => {
-    let checkBody = await getAllProducts();
-    expect(checkBody.body).toBe(JSON.stringify(productList));
-  });
+const setMockEvent = data => ({
+    Records: [
+        {
+            body: JSON.stringify(data),
+        },
+    ]
+    });
 
-  test('getAllProducts lambda: to be return status code 200', async () => {
-    let checkBody = await getAllProducts();
-    expect(checkBody.statusCode).toBe(200);
-  });
-
-  test('getAllProducts lambda: to be return define or not', async () => {
-    expect(await getAllProducts()).toBeDefined();
-  });
-
-  test('getProductsById lambda: to be return obj by Id', async () => {
-    let prodById = {
-      pathParameters:{
-        productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
-      }
+jest.mock('pg', () => {
+    const Client = {
+        connect: jest.fn(),
+        query: jest.fn(() => ({
+            rows: [{   
+                price: 99,
+                title: 'JEST TEST',
+                description: 'JEST DESC',
+                count: 99 
+            }],
+        })),
+        end: jest.fn(),
     };
+    return { Client: jest.fn(() => Client) };
+});
 
-    const exp = await getProductsById(prodById);
-    expect(exp.body).toBe(JSON.stringify(productList[7]));
-  });
+describe('Tests for catalogBatchProcess', () => {
 
-  test('getProductsById lambda: to be return status code by Id', async () => {
-    let prodById = {
-      pathParameters:{
-        productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
-      }
-    };
-    const exp = await getProductsById(prodById);
-    expect(exp.statusCode).toBe(200);
-  });
+    test('to be return the "succsess" if all OK', async () => {
+        const mockDataForDB = {
+            price: 99,
+            title: 'JEST TEST',
+            description: 'JEST DESC',
+            count: 99
+        }; 
 
-  test('getProductsById lambda: to be return define or not', async () => {
-    let prodById = {
-      pathParameters:{
-        productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
-      }
-    };
-    expect(await getProductsById(prodById)).toBeDefined();
-  });
+        AWSMock.setSDKInstance(AWS);
+        AWSMock.mock('SNS', 'publish', (params, callback) => {
+            callback(undefined, 'Data sent successfully'); // Mocked response returns ‘success’ always
+            });
+
+        const result = await catalogBatchProcess(setMockEvent(mockDataForDB));
+        const { message } = JSON.parse(result.body);
+        expect(message).toBe('Data sent successfully'); // Mocked response to equal to ‘success’ string from above
+
+        AWSMock.restore(); // Restore the mock
+      });
+
+      test('to be return the "status code 400" and message "Product data is invalid" if data is not valid',
+       async () => {
+           const mockDataForDB = {
+            title: 'JEST TEST',
+            description: 'JEST DESC',
+            count: 99           
+        }; 
+
+        const result = await catalogBatchProcess(setMockEvent(mockDataForDB));
+        const { message } = JSON.parse(result.body);
+        expect(result.statusCode).toBe(400);
+        expect(message).toBe('Product data is invalid');
+        AWSMock.restore();
+      });
+
+      test('to be return the "status code 500" and message "Unexpected error" if unexpected error',
+      async () => {
+          const mockDataForDB = {
+              price: 99,
+              title: 'JEST TEST',
+              description: 'JEST DESC',
+              count: 99           
+       }; 
+
+       AWSMock.setSDKInstance(AWS);
+       AWSMock.mock('SNS', 'publish', (params, callback) => {
+           callback(new Error('Something went wrong')); // Mocked response returns ‘success’ always
+           });
+
+       const result = await catalogBatchProcess(setMockEvent(mockDataForDB));
+       const { message } = JSON.parse(result.body);
+       expect(result.statusCode).toBe(500); 
+       expect(message).toBe('Unexpected error');
+       AWSMock.restore();
+     });
+});
 
 
 
 
+
+//  OLD TESTS
+
+// import { getProductsById } from "../handler/getProductsById";
+// import {getAllProducts}  from "../handler/getAllProducts";
+// import productList from '../handler/productList'; 
+// 
+//   test('getAllProducts lambda: get all product', async () => {
+//     let checkBody = await getAllProducts();
+//     expect(checkBody.body).toBe(JSON.stringify(productList));
+//   });
+// 
+//   test('getAllProducts lambda: to be return status code 200', async () => {
+//     let checkBody = await getAllProducts();
+//     expect(checkBody.statusCode).toBe(200);
+//   });
+// 
+//   test('getAllProducts lambda: to be return define or not', async () => {
+//     expect(await getAllProducts()).toBeDefined();
+//   });
+// 
+//   test('getProductsById lambda: to be return obj by Id', async () => {
+//     let prodById = {
+//       pathParameters:{
+//         productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
+//       }
+//     };
+// 
+//     const exp = await getProductsById(prodById);
+//     expect(exp.body).toBe(JSON.stringify(productList[7]));
+//   });
+// 
+//   test('getProductsById lambda: to be return status code by Id', async () => {
+//     let prodById = {
+//       pathParameters:{
+//         productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
+//       }
+//     };
+//     const exp = await getProductsById(prodById);
+//     expect(exp.statusCode).toBe(200);
+//   });
+// 
+//   test('getProductsById lambda: to be return define or not', async () => {
+//     let prodById = {
+//       pathParameters:{
+//         productId:'7567ec4b-b10c-45c5-9345-fc73c48a80a1'
+//       }
+//     };
+//     expect(await getProductsById(prodById)).toBeDefined();
+//   });
+// 
+// 
+// 
+// 
---
+++ product-service/handler/catalogBatchProcess.js
@@ -0,0 +1,81 @@
+import { SNS } from 'aws-sdk';
+import { dbOptions, Client } from './connection/connection'
+
+export const catalogBatchProcess = async (event) => {
+    
+    const client = new Client(dbOptions);
+    await client.connect();
+    console.log(`******CHECK EVENT  ${JSON.stringify(event)}`);
+    const items = await event.Records.map(({body}) => body );
+    
+    try {
+        for (let item of items) {
+            const { price, title, description, count } = JSON.parse(item);
+    
+            if (!price || !title || !description || !count) {
+                throw new Error(`Product data is invalid`);
+            };
+            await client.query('BEGIN');
+            const addReqToProductDB = `insert into products (title, description, price ) values ('${title}', '${description}', ${price}) returning id`;
+            const resFromProductDB = await client.query(addReqToProductDB);
+            const primaryKeyID = resFromProductDB.rows[0].id;
+            const addReqToStockDB =  `insert into stocks (product_id, count ) values ('${primaryKeyID}', ${count})`;
+            await client.query(addReqToStockDB);
+            await client.query('COMMIT');
+    }
+        const sns = new SNS({region: 'eu-west-1'});
+        const params = {
+            Subject: 'Products have been created in your DB',
+            Message: `${JSON.stringify(items)}`,
+            TopicArn: process.env.SNS_ARN
+        };
+        // console.log(`****Params: ${JSON.stringify(params)}`);
+        // console.log(`****ITEMS: ${JSON.stringify(items)}`);
+
+        const resFromSns = await sns.publish(params).promise();
+        // console.log(`******* RES FROM SNS: - ${resFromSns}`);
+
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({
+                message: resFromSns,
+            }),
+        };
+        
+
+    } catch (e) {
+        // console.log(`*****ERROR MESSAGE: ${e.message}`);
+        // console.log(`*****ERROR NAME: ${e.name}`);
+        await client.query('ROLLBACK');
+        if (e.message === 'Product data is invalid') {
+            console.log("*****Error SyntaxError: ", e);
+            return {
+                statusCode: 400,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({message: "Product data is invalid"}),
+        };
+        } else {
+            console.log("*****Error status 500: Unexpected error: ", e);
+            return {
+                statusCode: 500,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': true,
+                 },
+                 body: JSON.stringify({
+                 message: "Unexpected error"
+                }),
+            };
+        };
+    };
+};
+
+
+
