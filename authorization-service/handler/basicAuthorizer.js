b81c2d32fea77df4837bae5ac57677f0abe6be23
Pavel <pavelvaloshyn89@gmail.com>
2021-10-10 14:59:11 +0200

    refactor: added outputs for basicAuthorizerArn

--- authorization-service/handler/basicAuthorizer.js
+++ authorization-service/handler/basicAuthorizer.js
@@ -23,18 +23,11 @@ export const basicAuthorizer  = async (event, context, callback) => {
 
     try {
         const encoded = event.authorizationToken.split(' ')[1];
-        console.log(`******CHECK encoded: ${encoded}`);
         const buff = Buffer.from(encoded, 'base64');
-        console.log(`********CHECK BUFF: ${buff}`);
         const [ username, password ] = buff.toString('utf-8').split(':');
         const storedPassword = process.env[username];
-        console.log(`********CHECK username: ${username}`);
-        console.log(`********CHECK storedPassword: ${storedPassword}`);
-        console.log(`********CHECK PASSWORD: ${password}`);
         const effect = !storedPassword || storedPassword !== password ? 'Deny' : 'Allow';
-        console.log(`********CHECK EFFECT: ${effect}`);
         const policy = generatePolicy(encoded, event.methodArn, effect);
-        console.log(`CHECK POLICY: ${JSON.stringify(policy)}`);
         callback(null, policy);
     } catch (e) {
         console.log(`*******CHECK ERROR ${e}`);
