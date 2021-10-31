b4e6d9f5954f2d6ee1517cc195ef1e168d65aa59
Pavel <pavelvaloshyn89@gmail.com>
2021-10-31 19:37:36 +0100

    feat: added hanler product service and cart

--- bff-service/index.js
+++ bff-service/index.js
@@ -1,38 +1,62 @@
 import axios from 'axios';
-import Express, { response } from 'express';
+import express from 'express';
 import { config } from './common/config.js';
 // import cors from 'cors';
-const app = Express();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-console.log(config.PORT);
-
-app.use(Express.json());
-app.use(Express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // app.use(cors());
 
-app.all('/*', /* async */ (req, res) => {
-   
-    console.log(req.originalUrl);
+app.all('/*', (req, res) => {
+    const recipient = req.originalUrl.split('/')[1].split('?')[0];
+    const recipientURL = process.env[recipient];
+    // console.log(`CHECK DATA: ${JSON.stringify(req.body)}`)
 
-    const axiosConfig = {
-        method: 'get',
-        url: 'https://b6sld3tghb.execute-api.eu-west-1.amazonaws.com/dev/products',
-        // ...(Object.keys(req.body || {}).length > 0 && {data: req.body})
+    if (recipientURL) {
+        let finishedURL = '';
+        if (recipient === 'cart') {
+            finishedURL = recipientURL;
+        } else if (recipient === 'products') {
+            const productId = (req.query.productId) ? req.query.productId : '';
+            finishedURL  = `${recipientURL}/${recipient}/${productId}`;
+        }
+    // console.log(`*******: ${req.query.productId}`);
+    // console.log(`FIRST RECIPIENT: ${recipient}`);
+    // console.log(`RECIPIENT URL: ${recipientURL}`);
+    // console.log(`FINISHED URL: ${finishedURL}`);
+
+        const axiosConfig = {
+            method: req.method,
+            url: finishedURL,
+            data: Object.keys(req.body).length === 0 ? null : req.body,
+            ...(Object.keys(req.body || {}).length > 0 && {data: req.body})
+        }
+
+       console.log(`axiosConfig: ${JSON.stringify(axiosConfig)}`);
+
+       axios(axiosConfig)
+       .then((response) => {
+           console.log(`res from aws: ${response.data}`);
+           res.json(response.data)
+       })
+       .catch(error => {
+           console.log(`ERROR MESSAGE: ${JSON.stringify(error)}`);
+            if (error.response) {
+                const {
+                    status,
+                    data
+                } = error.response;
+                console.log(`CHECK STATUS: ${status}`)
+                res.status(status).json(data)
+            }
+       });
+
+    } else {
+        res.status(500).json({error: 'Cannot process request!'})
     }
-
-    console.log(`axiosConfig: ${JSON.stringify(axiosConfig)}`)
-    /* const response = await */ axios(axiosConfig)
-    // console.log(`RESPONSE FROM AWS: ${JSON.stringify(response)}`)
-   .then((response) => {
-       console.log(`res from aws: ${response.data}`);
-       res.json(response.data)
-   })
-   .catch(error => {
-       console.log(`ERROR: ${JSON.stringify(error)}`)
-   })
-    // res.json(response.data);
 });
 
 app.listen(PORT, () => {
