c983e1ded9dab7dac67c2974953fefed86fadfda
Pavel <pavelvaloshyn89@gmail.com>
2021-11-05 23:07:33 +0100

    refactor: changed PORT

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { config } from './common/config';
 
 const PORT = config.PORT || 3000;
-
+console.log(PORT);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
